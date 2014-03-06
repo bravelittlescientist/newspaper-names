ecdbd17eecb66b40229267c68c9543e4f19a09aa
Eugenia Gabrielova <genia.likes.science@gmail.com>
2014-03-06 10:42:44 -0800

    Initial commit

---
+++ newspaper_name_finder/settings.py
@@ -0,0 +1,15 @@
+# Scrapy settings for newspaper_name_finder project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'newspaper_name_finder'
+
+SPIDER_MODULES = ['newspaper_name_finder.spiders']
+NEWSPIDER_MODULE = 'newspaper_name_finder.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'newspaper_name_finder (+http://www.yourdomain.com)'
